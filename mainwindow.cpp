0cf1833ff7d8a6e7bf89f8f2e9ed37fb2cbacfcc
yuxuan <xuanyuong@gmail.com>
2025-04-02 00:38:42 +0800

    Implement media player controls

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,5 +1,6 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <cstdio>
 #include <QDir>
 #include <QString>
 #include <QRandomGenerator>
@@ -17,41 +18,34 @@
 
 // Page to reference for resizing GUI dynamically based on window size: https://doc.qt.io/qt-6/layout.html
 
-QDir dirImages;
-QStringList pathList;
-QString pathRand;
-
-// This set of instances needs to be declared on top of the file to ensure it doesn't get destroyed when the runtime reaches the end of a function.
-QMediaPlayer player;
-QAudioOutput *audio = new QAudioOutput;
-
-void onPlayerPositionChanged(qint64 position){
-    qint64 duration = player.duration();
-    if (duration - position < 100){
-        player.pause();
-    }
-}
-
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    ui->slrProgressBar->installEventFilter(this);
     QObject::connect(ui->btnGenerate, &QPushButton::clicked, this, &MainWindow::btnGenerate_clicked);
     QObject::connect(ui->btnSelectFolder, &QPushButton::clicked, this, &MainWindow::btnSelectFolder_clicked);
+    QObject::connect(ui->btnPlayPause, &QPushButton::clicked, this, &MainWindow::btnPlayPause_clicked);
+    QObject::connect(ui->btnRewind, &QPushButton::clicked, this, &MainWindow::btnRewind_clicked);
+    QObject::connect(ui->btnSkip, &QPushButton::clicked, this, &MainWindow::btnSkip_clicked);
+    QObject::connect(&player, &QMediaPlayer::positionChanged, this, &MainWindow::playerPositionChanged);
+    QObject::connect(ui->slrProgressBar, &QAbstractSlider::sliderPressed, this, &MainWindow::sliderPressed);
+    QObject::connect(ui->slrProgressBar, &QAbstractSlider::sliderMoved, this, &MainWindow::sliderMoved);
+    QObject::connect(ui->slrProgressBar, &QAbstractSlider::sliderReleased, this, &MainWindow::sliderReleased);
 
     // Initialize
-    QObject::connect(&player, &QMediaPlayer::positionChanged, &onPlayerPositionChanged);
     player.setVideoOutput(ui->vid);
     player.setAudioOutput(audio);
     ui->vid->hide();
     ui->img->hide();
-
+    ui->contPlayerPanel->hide();
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+    delete audio;
 }
 
 void MainWindow::btnGenerate_clicked()
@@ -78,7 +72,7 @@ void MainWindow::btnGenerate_clicked()
         return;
     }
     else pathRand = pathRandTemp;
-    QString fileExtension = QFileInfo(pathRand).suffix().toLower();  // Change all letters lowercase (eg. JPG to jpg)
+    QString fileExtension = QFileInfo(pathRand).suffix().toLower();  // Change all letters lowercase. (eg. JPG to jpg)
     if (fileExtension == "png" || fileExtension == "jpg" || fileExtension == "jpeg" || fileExtension == "jfif"){
         player.stop();
         QPixmap imgRand(pathRand);
@@ -87,25 +81,28 @@ void MainWindow::btnGenerate_clicked()
         ui->img->setPixmap(imgRand.scaled(width, height, Qt::KeepAspectRatio, Qt::SmoothTransformation));
         ui->img->setAlignment(Qt::AlignCenter);
         ui->vid->hide();
+        ui->contPlayerPanel->hide();
         ui->img->show();
     }
     else if (fileExtension == "gif" || fileExtension == "mp4" || fileExtension == "mkv"){
         ui->img->hide();
         player.setSource(QUrl(pathRand));
         ui->vid->show();
+        ui->contPlayerPanel->show();
         player.play();
     }
     else if (fileExtension == "mp3" || fileExtension == "wav"){
         ui->img->hide();
         player.setSource(QUrl(pathRand));
         ui->vid->show();
+        ui->contPlayerPanel->show();
         player.play();
     }
     ui->lblFilePath->setText("File Path: " + QFileInfo(pathRand).path());
     ui->lblFilePath->adjustSize();
     ui->lblFileName->setText("Name: " + QFileInfo(pathRand).fileName());
     ui->lblFileName->adjustSize();
-    ui->contActionPanel->setCurrentIndex(1);
+    ui->contPlayerPanel->setCurrentIndex(1);
 }
 
 void MainWindow::btnSelectFolder_clicked()
@@ -131,3 +128,85 @@ void MainWindow::btnSelectFolder_clicked()
     }
 }
 
+void MainWindow::btnPlayPause_clicked()
+{
+    if (player.isPlaying()){
+        player.pause();
+        ui->btnPlayPause->setText("Play");
+    }
+    else {
+        if (player.duration() - player.position() < 100) player.setPosition(0);  // Restart the video if the almost ended. (Paused by program due to reached the last few frame of video)
+        player.play();
+        ui->btnPlayPause->setText("Pause");
+    }
+}
+
+void MainWindow::btnRewind_clicked()
+{
+    qint64 newPosition = player.position() - 10000;
+    player.setPosition(newPosition);  // Sets to 0 if it is a negative value.
+}
+
+void MainWindow::btnSkip_clicked()
+{
+    qint64 newPosition = player.position() + 30000;
+    player.setPosition(newPosition);  // Sets to last millisecond of duration if the value is higher than duration.
+}
+
+QString formatTime(qint64 ms) {
+    int hours = (ms / 3600000);
+    int mins = (ms / 60000) % 60;
+    int secs = (ms / 1000) % 60;
+    char buffer[9];
+    std::sprintf(buffer, "%02d:%02d:%02d", hours, mins, secs);  // Produces either 01:01:01 or 101:59:59. (Meaning hour can exceed 100 (very unlikely to happen during usage))
+    return QString::fromUtf8(buffer);
+}
+
+void MainWindow::playerPositionChanged(qint64 position){
+    qint64 duration = player.duration();
+    if (duration - position < 100){
+        player.pause();
+        ui->btnPlayPause->setText("Play");
+    }
+    ui->slrProgressBar->setMaximum(player.duration() - 100);  // Offset for last-frame pause.
+    ui->slrProgressBar->setValue(position);
+    ui->lblCurrentTime->setText(formatTime(position));
+    ui->lblRemainingTime->setText(formatTime(duration - position));
+}
+
+void MainWindow::sliderPressed() {
+    player.pause();
+}
+
+void MainWindow::sliderMoved(int value) {
+    player.setPosition(value);
+}
+
+void MainWindow::sliderReleased() {
+    player.play();
+}
+
+bool MainWindow::eventFilter(QObject *obj, QEvent *event) {
+    if (obj == ui->slrProgressBar && event->type() == QEvent::MouseButtonPress) {
+        Qt::MouseButtons btns = QApplication::mouseButtons();  // Gets the current state of all mouse buttons.
+        QPoint localPos = ui->slrProgressBar->mapFromGlobal(QCursor::pos());  // Get the local coordinate of the slider. (Eg. The value will be negative if the mouse is to the left or top of the slider)
+        bool clickOnSlider = (btns & Qt::LeftButton) && (ui->slrProgressBar->rect().contains(localPos));  // Checking whether the mouseclick position is within the slider widget (4 sides of rectangle).
+        if (clickOnSlider) {
+            // Get coordinate of the player's current position. (Conversion to float is necessary to calculate ratios.)
+            float currentPos = static_cast<float>(player.position());
+            float currentDuration = static_cast<float>(player.duration());
+            float slrWidth = static_cast<float>(ui->slrProgressBar->width());
+            float currentRatio = currentPos / currentDuration;
+            int currentXCoordinate = slrWidth * currentRatio;
+            int lowerBound = currentXCoordinate - 4;
+            int higherBound = currentXCoordinate + 4;
+            if (localPos.x() < lowerBound || localPos.x() > higherBound) {  // Skip event filter if the user wants to click on the slider head.
+                float posRatio = static_cast<float>(localPos.x()) / slrWidth;  // Get the current clicked position on the slider in relation to its entire bar. (x coordinate = horizontal progress bar) (how many duration passed over (/) the total duration)
+                int newSliderPos = player.duration() * posRatio;  // Multiply the ratio with the total media duration to get the new time position that the user have clicked on.
+                player.setPosition(newSliderPos);
+                return true;
+            }
+        }
+    }
+    return QMainWindow::eventFilter(obj, event);
+}
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,9 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QDir>
+#include <QMediaPlayer>
+#include <QAudioOutput>
 
 QT_BEGIN_NAMESPACE
 namespace Ui {
@@ -19,13 +22,27 @@ public:
 
 private slots:
     void btnGenerate_clicked();
-
     void btnSelectFolder_clicked();
+    void btnPlayPause_clicked();
+    void btnRewind_clicked();
+    void btnSkip_clicked();
+    void playerPositionChanged(qint64 position);
+    void sliderPressed();
+    void sliderMoved(int value);
+    void sliderReleased();
 
 signals:
     // Any custom signals
 
-private:
+protected:
+    bool eventFilter(QObject *obj, QEvent *event) override;
+
+private:  // Global variables
     Ui::MainWindow *ui;
+    QAudioOutput *audio = new QAudioOutput;
+    QMediaPlayer player;  // This set of player + audio instances needs to be declared on top of the file to ensure it doesn't get destroyed when the runtime reaches the end of a function.
+    QDir dirImages;
+    QStringList pathList;
+    QString pathRand;
 };
 #endif // MAINWINDOW_H
